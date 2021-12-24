26f3b8fbdb6aac9ab08d95d7d6270034f14cc497
mansion chieng <mansion@pop-os.localdomain>
2021-12-24 18:52:52 +0800

    一个简单的质数结论

---
+++ CCPC 2021 Qualification Round/d.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long v, t;
+    cin >>t;
+    while(t--) {
+        cin >>v;
+        if (v >= 2) {
+            cout <<"NO" << endl;
+        }else cout << "YES" << endl;
+    }
+}
